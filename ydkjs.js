f527a5160474017fd4cdaeb084df66c7e02f2d69
MuhamadVicky <vickyymuhamad@gmail.com>
2018-08-13 14:27:17 +0700

    Updated the solution - adding prompt so user can input the Bank Account

--- ydkjs.js
+++ ydkjs.js
@@ -1,31 +1,37 @@
-var bankAccount = 220;
+var bankAccount = 0;
 let phone1Price = 200;
 let accessory1Price = 9.99;
 let taxRate = 0.05;
 let spendingThreshold = 0;
 var a  = true;
+var number = 1;
 
+bankAccount = prompt("please add your bank account balance.");
 while (a) {
-  calculatePhone = calculate(phone1Price, accessory1Price);
-  console.log(Math.round(calculatePhone) + " $");
+  var calculatePhone = calculate(phone1Price, accessory1Price);  
+  console.log((calculatePhone).toFixed(2) + " $");
   checkBalance(calculatePhone);
   
 }
 
 function calculate(phonePrice,accessory){
-  spendingThreshold = phonePrice + accessory;
-  let tax = spendingThreshold + (spendingThreshold * taxRate);
-  return tax;  
+  total =  (phonePrice + accessory);
+  let tax = total + (spendingThreshold * taxRate);
+  return (tax);  
 }
 
 
 function checkBalance(calculatePhone){
-  if (calculatePhone < bankAccount){
+  if (bankAccount >= Math.round(calculatePhone)){
     bankAccount = bankAccount - calculatePhone;
+    spendingThreshold += (calculatePhone);
+    console.log("Purchase Order " + number + " Completed");
+    number += 1;
   }
   else {
     console.log("You dont have insufficient balance!");
-    console.log("Your balance is " + Math.round(bankAccount) + " $");
+    console.log("Your balance is " + (bankAccount).toFixed(2) + " $");
+    console.log("Your spending is " + spendingThreshold.toFixed (2));
     a = false;
   }
 }
